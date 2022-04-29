0a587275eb635ed30b9de40c7ec420da5884f0ff
Bocharov Denis <mr.ybrik@gmail.com>
2022-04-29 09:34:26 +0300

    Database

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -73,10 +73,24 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DB_NAME = "db_hotel"
+DB_USER = "postgres"
+DB_PASSWORD ="qwe123"
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
